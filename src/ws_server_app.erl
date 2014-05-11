1b51abdc7a8bb955edb3178a79aeca3d8843dbc7
Weerasak Chongnguluam <singpor@gmail.com>
2014-05-11 20:28:44 +0700

    add start all and create ets table

--- src/ws_server_app.erl
+++ src/ws_server_app.erl
@@ -5,14 +5,22 @@
 
 -define(C_ACCEPTORS,  100).
 
+start_all() ->
+    application:start(ranch),
+    application:start(crypto),
+    application:start(cowlib),
+    application:start(cowboy),
+    application:start(hello_cowboy).
+
 start(_StartType, _StartArgs) ->
+    ets:new(ws_client, [set, named_table, public]),
     Routes    = routes(),
     Dispatch  = cowboy_router:compile(Routes),
     Port      = port(),
     TransOpts = [{port, Port}],
     ProtoOpts = [{env, [{dispatch, Dispatch}]}],
     {ok, _}   = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
-    erlblog_sup:start_link().
+    ws_server_sup:start_link().
 
 stop(_State) ->
     ok.
